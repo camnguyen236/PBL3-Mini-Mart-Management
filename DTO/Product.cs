dd1934dcb8a06e10efc7a4215b65202bbc365cde
xenicedtea <lvd.levandat@gmail.com>
2022-04-19 23:58:30 +0700

    add product

---
+++ DTO/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Product
+    {
+        public int ID_P { get; set; }
+        public string ID_PG { get; set; }
+        public string Name_P { get; set; }
+        public string Unit_P { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Price_P { get; set; }
+        public string VAT { get; set; }
+        public string Cost_P { get; set; }
+    }
+}
