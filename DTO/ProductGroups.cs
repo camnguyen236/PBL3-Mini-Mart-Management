65f4225ae23de4e34c84520835b57d225666777f
xenicedtea <lvd.levandat@gmail.com>
2022-04-20 11:02:49 +0700

    add ProductGroup class

---
+++ DTO/ProductGroups.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class ProductGroups
+    {
+        public string ID_PG { get; set; }
+        public string Name_PG { get; set; }
+    }
+}
